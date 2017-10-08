6a5404dd496e3bb0668f373b90e5a2c63b504882
Farhandroid <32593150+Farhandroid@users.noreply.github.com>
2017-10-09 01:27:44 +0600

    NavigationDrawer Added

--- app/src/main/java/tanvir/busmanagementsystem/MainActivity.java
+++ app/src/main/java/tanvir/busmanagementsystem/MainActivity.java
@@ -1,31 +1,56 @@
 package tanvir.busmanagementsystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
-import android.widget.Toast;
 
 import com.sdsmdg.tastytoast.TastyToast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawerLayout)DrawerLayout drawerLayout;
+    @BindView(R.id.navigationView)NavigationView navigationView;
+    @BindView(R.id.toolbarlayoutinmainactivity)Toolbar toolbar;
+
+
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    FragmentTransaction fragmentTransaction;
+
+    private Menu appbar_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open
+                ,R.string.drawer_close);
+
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        Menu nav_Menu = navigationView.getMenu();
+        nav_Menu.findItem(R.id.HOmeId).setChecked(true);
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    public void onPostCreate(Bundle savedInstanceState)
     {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_item, menu);
-        return true;
+        super.onPostCreate(savedInstanceState);
+        actionBarDrawerToggle.syncState();
     }
-
 
 
 
